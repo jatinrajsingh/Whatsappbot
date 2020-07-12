041c2431a1214d93d4eedaf661e582d155f9255b
Jatin Thakur <jatinrajsingh360@gmail.com>
2020-07-12 13:25:54 +0530

    added file

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
+from utils import fetch_reply
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World"
+
+
+@app.route("/sms", methods=['POST'])
+def sms_reply():
+    msg = request.form.get('Body')
+    phone_no = request.form.get('From')
+    reply = fetch_reply(msg, phone_no)
+
+    resp = MessagingResponse()
+    resp.message(reply)
+
+    return str(resp)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
